f5a4a15c0f94af5a784a36ae6ca7d8d4a4c81702
Yazid Ismail <flight846@gmail.com>
2019-05-29 12:50:00 +0800

    Add notifications cloud functions and display

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -11,7 +11,7 @@ class CreateProject extends Component {
 
     handleChange = (e) => {
         this.setState({
-        [e.target.id]: e.target.value
+            [e.target.id]: e.target.value
         })
     }
 
